f03ad4ebecbe7b4b4c7f7553c40f4f5eec0b311a
Naveed Khowaja <naveedhussain.khowaja@abtach.org>
2019-09-09 12:09:31 +0500

    implemented different spinner collection

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import { StyleSheet, Modal, View, ActivityIndicator } from "react-native";
 import PropTypes from "prop-types";
-
+import {
+  BallIndicator,
+  BarIndicator,
+  DotIndicator,
+  MaterialIndicator,
+  PacmanIndicator,
+  PulseIndicator,
+  SkypeIndicator,
+  UIActivityIndicator,
+  WaveIndicator,
+} from './indicators';
 import SpinnerManager from "./SpinnerManager";
 
 /**x
  * Simple random ID for internal Spinner component usage
  */
 function srid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
 
-    return `${s4()}-${s4()}-${s4()}`;
+  return `${s4()}-${s4()}-${s4()}`;
 }
 
 /**
@@ -24,11 +34,11 @@ function srid() {
  *  showSpinner()
  * ```
  */
-export function showSpinner() {
-    const ref = SpinnerManager.getDefault();
-    if (!!ref) {
-        ref.showSpinner();
-    }
+export function showSpinner(...args) {
+  const ref = SpinnerManager.getDefault();
+  if (!!ref) {
+    ref.showSpinner(...args);
+  }
 }
 
 /**
@@ -39,10 +49,10 @@ export function showSpinner() {
  * ```
  */
 export function hideSpinner() {
-    const ref = SpinnerManager.getDefault();
-    if (!!ref) {
-        ref.hideSpinner();
-    }
+  const ref = SpinnerManager.getDefault();
+  if (!!ref) {
+    ref.hideSpinner();
+  }
 }
 
 /**
@@ -58,126 +68,151 @@ export function hideSpinner() {
  * ```
  */
 export default class Spinner extends Component {
-    static defaultProps = {
-        /**
-         * Use to handle if the instance can be registed as default/global instance
-         */
-        canRegisterAsDefault: true,
-        color: ""
-    };
-    static propTypes = {
-        canRegisterAsDefault: PropTypes.bool,
-        color: PropTypes.string
-    };
-    state = {
-        isShowing: false
-    };
-    constructor(props) {
-        super(props);
-
-        if (!this._id) this._id = srid();
-    }
-    componentDidMount() {
-        if (this.props.canRegisterAsDefault) {
-            SpinnerManager.register(this);
-        }
-    }
-    componentWillUnmount() {
-        if (this.props.canRegisterAsDefault) {
-            SpinnerManager.unregister(this);
-        }
-    }
-
+  SpinnerComponent = null
+  static defaultProps = {
     /**
-     * Instace ref function to handle show spinner
-     *
-     * ```
-     * this.refs.YOUR_REF.showSpinner()
-     * ```
+     * Use to handle if the instance can be registed as default/global instance
      */
-    showSpinner() {
-        this.setState({ isShowing: true });
+    canRegisterAsDefault: true,
+    color: ""
+  };
+
+  static propTypes = {
+    canRegisterAsDefault: PropTypes.bool,
+    color: PropTypes.string
+  };
+
+  state = {
+    isShowing: false,
+  };
+
+  constructor(props) {
+    super(props);
+
+    if (!this._id) this._id = srid();
+  }
+
+  componentDidMount() {
+    const { type } = this.props
+
+    if (this.props.canRegisterAsDefault) {
+      SpinnerManager.register(this);
     }
-    /**
-     * Instace ref function to programmatically hide spinner
-     *
-     * ```
-     * this.refs.YOUR_REF.hideSpinner()
-     * ```
-     */
-    hideSpinner() {
-        this.setState({ isShowing: false });
+  }
+
+  componentWillUnmount() {
+    if (this.props.canRegisterAsDefault) {
+      SpinnerManager.unregister(this);
     }
-    render() {
-        return (
-            <Modal
-                onRequestClose={() => console.log("spinner closed")}
-                animationType="none"
-                supportedOrientations={["landscape", "portrait"]}
-                transparent
-                visible={this.state.isShowing}
-            >
-                <View
-                    style={[
-                        styles.container,
-                        { backgroundColor: "rgba(0, 0, 0, 0.25)" }
-                    ]}
-                    key={`spinner_${Date.now()}`}
-                >
-                    <View style={styles.background}>
-                        <ActivityIndicator
-                            size="large"
-                            color={this.props.color}
-                        />
-                    </View>
-                </View>
-            </Modal>
-        );
+  }
+
+  render() {
+    return (
+      <Modal
+        onRequestClose={() => console.log("spinner closed")}
+        animationType="fade"
+        supportedOrientations={["landscape", "portrait"]}
+        transparent
+        visible={this.state.isShowing}>
+        <View style={[styles.container, { backgroundColor: "rgba(0, 0, 0, 0.25)" }]}>
+          {this.getSpinnerComponent()}
+        </View>
+      </Modal>
+    );
+  }
+
+  /**
+   * Instace ref function to handle show spinner
+   *
+   * ```
+   * this.refs.YOUR_REF.showSpinner()
+   * ```
+   */
+  showSpinner(args) {
+    this.setState({ isShowing: true });
+  }
+
+  /**
+   * Instace ref function to programmatically hide spinner
+   *
+   * ```
+   * this.refs.YOUR_REF.hideSpinner()
+   * ```
+   */
+  hideSpinner() {
+    this.setState({ isShowing: false });
+  }
+
+  getSpinnerComponent() {
+    const { type } = this.props
+    switch (type) {
+      case 'BallIndicator':
+        return <BallIndicator {...this.props} />;
+      case 'BarIndicator':
+        return <BarIndicator {...this.props} />;
+      case 'DotIndicator':
+        return <DotIndicator {...this.props} />;
+      case 'MaterialIndicator':
+        return <MaterialIndicator {...this.props} />;
+      case 'PacmanIndicator':
+        return <PacmanIndicator {...this.props} />;
+      case 'PulseIndicator':
+        return <PulseIndicator {...this.props} />;
+      case 'SkypeIndicator':
+        return <SkypeIndicator {...this.props} />;
+      case 'UIActivityIndicator':
+        return <UIActivityIndicator {...this.props} />;
+      case 'WaveIndicator':
+        return <WaveIndicator {...this.props} />;
+      default:
+        return <ActivityIndicator />;
     }
-    _id;
+  }
+
+  _id;
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "transparent",
-        position: "absolute",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
-    },
-    background: {
-        position: "absolute",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    spinner: {
-        position: "relative",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    textContainer: {
-        flex: 1,
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        justifyContent: "center",
-        alignItems: "center",
-        position: "absolute"
-    },
-    textContent: {
-        top: 80,
-        height: 50,
-        fontSize: 20,
-        fontWeight: "bold"
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  background: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  spinner: {
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textContainer: {
+    flex: 1,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute"
+  },
+  textContent: {
+    top: 80,
+    height: 50,
+    fontSize: 20,
+    fontWeight: "bold"
+  }
 });
---
+++ src/indicators/index.js
@@ -0,0 +1,23 @@
+import Indicator from './indicator';
+import BallIndicator from './ball-indicator';
+import BarIndicator from './bar-indicator';
+import DotIndicator from './dot-indicator';
+import MaterialIndicator from './material-indicator';
+import PacmanIndicator from './pacman-indicator';
+import PulseIndicator from './pulse-indicator';
+import SkypeIndicator from './skype-indicator';
+import UIActivityIndicator from './ui-activity-indicator';
+import WaveIndicator from './wave-indicator';
+
+export {
+  Indicator,
+  BarIndicator,
+  BallIndicator,
+  DotIndicator,
+  MaterialIndicator,
+  PacmanIndicator,
+  PulseIndicator,
+  SkypeIndicator,
+  UIActivityIndicator,
+  WaveIndicator,
+};
